d70176384adb16303990c2db5ce8b75d1b805917
Ben <71155331+bchussid@users.noreply.github.com>
2024-02-14 10:44:16 -0500

    edit/styles

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { Button } from "./ui/button";
 const GetInTouch = () => {
   return (
-    <section>
+    <section className="py-16 bg-tertiary dark:bg-secondary/40">
       <div className='container mx-auto'>
         <div className='flex flex-col items-center'>
           <h2 className='h2 max-w-xl text-center mb-8'>
