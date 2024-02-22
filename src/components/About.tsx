aa8c6a76e3f7ca2fc999a58cf9f3733b2cd25461
Ben <71155331+bchussid@users.noreply.github.com>
2024-02-22 16:52:45 -0500

    add/new skills layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -77,6 +77,25 @@ const skillData = [
   },
 ];
 
+const skillData2 = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Tailwind",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "Firebase",
+  "Git",
+  "GitHub",
+  "Gitlab",
+  "Jira",
+  "Vue",
+  "SQL",
+];
+
 const qualificationData = [
   {
     title: "education",
@@ -222,19 +241,17 @@ const About = () => {
                           }
                         )}
                       </div>
-                      {/* education */}
-                      <div>education</div>
                     </div>
                   </div>
                 </TabsContent>
-                {/* skills */}
-                <TabsContent value="skills">
+                {/* skills - old */}
+                {/* <TabsContent value="skills">
                   <div className="text-center xl:text-left">
                     <h3 className="h3 mb-8">Tools I use</h3>
                     <div className="mb-16">
                       <h4 className="text-xl font-semibold mb-2">Skills</h4>
                       <div className="border-b border-border mb-4"></div>
-                      {/* skills list */}
+                      
                       <div>
                         {getData(skillData, "skills")?.data.map(
                           (item: { name: string }, index: number) => (
@@ -244,6 +261,22 @@ const About = () => {
                           )
                         )}
                       </div>
+                    </div>
+                  </div>
+                </TabsContent> */}
+                {/* skills - new */}
+                <TabsContent value="skills">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">Tools I use</h3>
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* skills list */}
+                      <ul className="flex flex-wrap justify-center gap-2 text-base">
+                        {skillData2.map((item, index) => (
+                          <li className="border dark:border-primary border-black/[0.1] rounded-lg px-5 py-3" key={index}>{item}</li>
+                        ))}
+                      </ul>
                     </div>
                   </div>
                 </TabsContent>
